5d9c50b7c27f605a97e9769b7ae9b6c09e564f64
AwallenceCode <awallencecode@gmail.com>
2025-09-26 16:57:05 +0300

    add menu for user

--- WeeklyPlanner/Program.cs
+++ WeeklyPlanner/Program.cs
@@ -7,37 +7,18 @@ class MyClass
     /// <summary>
     /// Method for filling in daily tasks by the user 
     /// </summary>
-    static void ChoiceDayOfWeek(string[][] userTasks)
+    static void ChoiceDayOfWeek()
     {
         Console.Write("Write the day of the week you need to record the task -> ");
-        
+
         string userDayChoice = Console.ReadLine();
-        
-        if (Enum.TryParse(userDayChoice, true, out DayOfWeek dayForTask));
+
+        if (Enum.TryParse(userDayChoice, true, out DayOfWeek dayForTask)) ;
         {
-            switch (userDayChoice)
-            {
-                case "0":
-                    userTasks[0][];
-                    break;
-                case "1":
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                case "5":
-                    break;
-                case "6":
-                    break;
-                default:
-                    break;
-            }
+            AddTaskToDay();
         }
-        
-        else { Console.WriteLine("There is no such day of the week"); }
+
+    //else { Console.WriteLine("There is no such day of the week"); }
     }
     
     
@@ -50,7 +31,7 @@ class MyClass
     /// <summary>
     /// method for removing tasks from the day
     /// </summary>
-    static void DeleteTaskToDay()
+    static void RemoveTaskToDay()
     {}
     
     /// <summary>
@@ -58,17 +39,58 @@ class MyClass
     /// </summary>
     static void ShowWeeklySchedule()
     {}
+
     static void Main()
     {
         Console.WriteLine("Welcome to your weekly planner!" +
                           "\tIn this planner, you can write down the day of the week on which you need to add or remove tasks." +
-                          "\nYou can also print out your full schedule for the week.");    
-        
+                          "\nYou can also print out your full schedule for the week.");
+
         const byte sizeColumn = 7;
         byte sizeLine;
 
         string[][] userTasks = new string[sizeColumn][];
 
-        ChoiceDayOfWeek(userTasks);
+        ChoiceDayOfWeek();
+
+        while (true)
+        {
+            Console.WriteLine("\nPress \"1\" - for add task" +
+                              "\nPress \"2\" - for remove task" +
+                              "\nPress \"3\" - for showing task" +
+                              "\nWrite \"Exit\" for exit the program");
+
+            Console.Write("\nEnter your choice ->");
+            string userChoice = Console.ReadLine()?.ToLower();
+
+            switch (userChoice)
+            {
+                case "1":
+                {
+                    ChoiceDayOfWeek();
+                    break;
+                }
+
+                case "2":
+                {
+                    RemoveTaskToDay();
+                    break;
+                }
+
+                case "3":
+                {
+                    ShowWeeklySchedule();
+                    break;
+                }
+                
+                case "exit":
+                {
+                    return;
+                }
+
+                default:
+                    Console.WriteLine("Try again..."); break;
+            }
+        }
     }
 }
